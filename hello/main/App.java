b48094dd5bcf48f8441c399150bc6760701035c0
puntogil <puntogil@libero.it>
2021-12-21 11:18:07 +0100

    primo commit

---
+++ hello/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+public class App {
+
+        public static void main(String[] args){
+                System.out.println("Hello");
+        }
+
+	/*public static int somma(int a, int b){
+
+        }*/
+}
